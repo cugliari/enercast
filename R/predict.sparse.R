327fbbdfcf129045b1a9e55fddabfb9a5071541c
Ignacio Ramirez <nacho@fing.edu.uy>
2016-08-19 22:05:47 -0300

    Nueva ver

--- R/predict.sparse.R
+++ R/predict.sparse.R
@@ -1,7 +1,7 @@
 # DEPENDENCY: Julien Mairal SPAMS for R
 library("spams")
 
-predict.sparse <- function(sp,new.data){
+predict.sparse <- function(sp,new.data) {
   if(inherits(sp,'sparse_class',TRUE) != 1)
     stop('Object is not of sparse_class')
   
@@ -12,27 +12,35 @@ predict.sparse <- function(sp,new.data){
   T = 24 # period, could be a parameter in the future
   N <- n / T 
   M = T*m
-  #
-  # normalize this variable to be N(0,Identity)
-  #
   X <- matrix(nrow=M,ncol=N)
   for (i in 1:m) {
     Xi <- new.data[,i]
-#    dim(Xi) <- c(T,N)
-#    for (j in 1:N) {
-#      Xi[,j] <- solve(sp$scaling[[i]],Xi[,j] - sp$centering[[i]])
-#    }
+    #    for (j in 1:N) {
+    #      Xi[,j] <- solve(sp$scaling[[i]],Xi[,j] - sp$centering[[i]])
+    #    }
     dim(Xi) <- c(T,N)
     X[((i-1)*T+1):(i*T),] <- Xi
   }
+  #
+  # trick: normalize but by twice the variance
+  # we don't know the variance of the part to be estimated, but that can be assumed to be the same
+  # as that of the part we do have.
+  cm <- apply(X,2,mean)
+  cv <- apply(X,2,sd)
+  for (j in 1:N) {
+    X[,j] <- ( X[,j] - cm[j]) / (2.*cv[j])
+  }
   A <- spams.lasso(X,sp$Dtoday,lambda1=sp$lambda,mode='PENALTY')
   X1 <- sp$Dtomorrow %*% A
+  for (j in 1:N) {
+    X1[,j] <- 2.*cv[j]*X1[,j] + cm[j]
+  }
+  
   for (j in 1:N) {
     Xj <- X1[,j]
     for (i in 1:m) {
       Xji <- Xj[((i-1)*T+1):(i*T)]
       X1[((i-1)*T+1):(i*T),j] <- Xji
-      #X1[((i-1)*T+1):(i*T),j] <- sp$scaling[[i]] %*% Xji + sp$centering[[i]]
     }
   }
   return(X1)
