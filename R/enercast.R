8b85764201c678956465574067e1a5e4b3d834e7
Jairo Cugliari <Jairo.Cugliari@univ-lyon2.fr>
2017-07-26 15:42:10 +0200

    add package description on doc

---
+++ R/enercast.R
@@ -0,0 +1,12 @@
+#' enercast: A package for predicting electricity load.
+#'
+#' The enercast package provides different models to predict the daily electricity load. Models covers
+#' both endogenous and exogenous variants (e.g. using or not external information as for instance
+#' meteorological variables).
+#' 
+#' @section enercast models:
+#' hong, ssm, ...
+#'
+#' @docType package
+#' @name enercast
+NULL
